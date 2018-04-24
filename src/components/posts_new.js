16d32a61088388746433b86ab4e30ae378403186
Zac McClung <zac.mcclung370@gmail.com>
2018-04-24 13:11:16 -0600

    showing errors to users

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,6 +11,7 @@ class PostsNew extends Component {
         {/* onBlur={field.input.onBlur} */}
         {/* ^^^ same as vvv */}
         <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
       </div>
     );
   }
