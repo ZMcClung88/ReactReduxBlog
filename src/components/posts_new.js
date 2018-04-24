67f93e5d31c2596639c53fbb72d8e59de7b71aa3
Zac McClung <zac.mcclung370@gmail.com>
2018-04-23 21:28:19 -0600

    generalizing fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        {/* onChange={field.input.onChange} */}
+        {/* onFocus={field.input.onFocus} */}
+        {/* onBlur={field.input.onBlur} */}
+        {/* ^^^ same as vvv */}
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+  }
   render() {
     return (
-      <form>
-        <Field
-          name="title"
-          component={}
-        />
-      </form>
+      <div>
+        <form>
+          <Field label="Title for posts" name="title" component={this.renderField} />
+        </form>
+        <form>
+          <Field label="Tags" name="tags" component={this.renderField} />
+        </form>
+        <form>
+          <Field label="Post Content" name="content" component={this.renderField} />
+        </form>
+      </div>
     );
   }
 }
