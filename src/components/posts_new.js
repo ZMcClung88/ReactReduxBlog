5101961e244de439377f81b3060b81cebe9080ee
Zac McClung <zac.mcclung370@gmail.com>
2018-04-24 21:06:13 -0600

    cancel post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
   renderField = field => {
@@ -29,6 +30,9 @@ class PostsNew extends Component {
           {' '}
           Submit
         </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
