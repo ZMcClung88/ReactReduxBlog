a2b40d2da39dc7129ee3b1a739f683a8214249a5
Zac McClung <zac.mcclung370@gmail.com>
2018-04-24 21:44:21 -0600

    navigation through callbacks

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -19,7 +19,10 @@ class PostsNew extends Component {
   };
   onSubmit(values) {
     console.log('on submit values', values);
-    this.props.createPost(values);
+
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
   render() {
     const { handleSubmit } = this.props;
