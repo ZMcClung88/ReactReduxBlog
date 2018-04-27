723829050c6c0804aba6f269b35081150b3019dd
Zac McClung <zac.mcclung370@gmail.com>
2018-04-26 22:33:46 -0600

    data dependencies

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -4,15 +4,28 @@ import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
-    const { id } = this.props.match.params;
+    const { id } = this.props.match.params; // <-- provided directly from react router
     this.props.fetchPost(id);
   }
   render() {
-    return <div>Posts Show!</div>;
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      // <div>Posts Show!</div>
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
   }
 }
 
 function mapStateToProps({ posts }, ownProps) {
-  return { posts: posts[ownProps.match.params.id] };
+  return { posts: posts[ownProps.match.params.id] }; // returns one specific post vs all posts
 }
 export default connect(mapStateToProps, { fetchPost })(PostsShow);
